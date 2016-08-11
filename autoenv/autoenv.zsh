19b7771d3baf8ba7b5f81d98ff085c2c1ce637e5
Noah Crocker <n@ncrocker.codes>
2016-08-10 21:40:14 -0700

    updates, and removed spacemacs to add again later

--- autoenv/autoenv.zsh
+++ autoenv/autoenv.zsh
@@ -1,1 +1,6 @@
-source /usr/local/opt/autoenv/activate.sh
+if [[ -f /usr/local/opt/autoenv/activate.sh ]];
+then
+  source /usr/local/opt/autoenv/activate.sh
+else
+  echo "Couldn't source autoenv. Is it installed?"
+fi
--- python/virtualenvwrapper.zsh
+++ python/virtualenvwrapper.zsh
@@ -1,4 +1,5 @@
 export WORKON_HOME=~/dev/pythonenvs
+mkdir -p $WORKON_HOME
 if [[ -f /usr/local/bin/virtualenvwrapper.sh ]];
 then
   source /usr/local/bin/virtualenvwrapper.sh
